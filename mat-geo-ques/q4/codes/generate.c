cbd363a3e9e1660c913aa6ff92a2c42311049a1a
= <=>
2024-08-27 13:01:57 +0530

    q4-changes

--- mat-geo-ques/q4/codes/generate.c
+++ mat-geo-ques/q4/codes/generate.c
@@ -1,8 +1,6 @@
-// line_points.c
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to generate 1000 points on the line joining (x1, y1, z1) and (x2, y2, z2)
 void generate_line_points(double x1, double y1, double z1, double x2, double y2, double z2, double *points, int num_points) {
     double dx = x2 - x1;
     double dy = y2 - y1;
--- mat-geo-ques/q4/codes/plot.py
+++ mat-geo-ques/q4/codes/plot.py
@@ -2,11 +2,8 @@ import ctypes
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the C library
-# Replace 'libline_points.so' with 'libline_points.dylib' or 'line_points.dll' as needed
 gen = ctypes.CDLL('./generate.so')
 
-# Define the argument and return types of the C function
 gen.generate_line_points.argtypes = [
     ctypes.c_double, ctypes.c_double, ctypes.c_double,
     ctypes.c_double, ctypes.c_double, ctypes.c_double,
@@ -15,24 +12,19 @@ gen.generate_line_points.argtypes = [
 
 gen.generate_line_points.restype = None
 
-# Define the points on the line
 x1, y1, z1 = 2, 3, 4
 x2, y2, z2 = -1, -2, 1
 x3, y3, z3 = 5, 8 ,7
-# Number of points
 num_points = 1000
 
-# Create an array to hold the points
 points = np.zeros((num_points, 3), dtype=np.double)
 
-# Call the C function
 gen.generate_line_points(
     x1, y1, z1, x2, y2, z2,
     points.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
     num_points
 )
 
-# Plot the points
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='r', marker='o')
