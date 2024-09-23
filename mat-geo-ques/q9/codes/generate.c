f411c8bd52f249797059804baae3c0000b64d3ec
= <=>
2024-09-23 18:09:47 +0530

    q9-changes

--- mat-geo-ques/q9/codes/generate.c
+++ mat-geo-ques/q9/codes/generate.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 struct point {
-	double x;
-	double y;
+    double x;
+    double y;
 };
+
 struct triangle {
-	double len;
-	struct point vertex1;
-	struct point vertex2;
-	struct point vertex3;
-	int num_points;
-	struct point **sides;
+    double length;
+    struct point vertex1;
+    struct point vertex2;
+    struct point vertex3;
+    int num_points;
+    struct point **sides;
 };
 struct point *get_vertex(struct point point1, struct point point2) {
+    struct point *vertex = malloc(sizeof(struct point));
+    double vec_x = point2.x - point1.x;
+    double vec_y = point2.y - point1.y;
     
-	double vec_x = point2.x - point1.x;
-	double vec_y = point2.y - point1.y;
-	double cos_val = sqrt(3) / 2;
-	double sin_val = 0.5;
-        struct point *vertex ;
-	vertex -> x = cos_val * vec_x - sin_val * vec_y;  vertex -> y = (-1) * (cos_val) * vec_y + sin_val * vec_x;
-	return vertex;
+    vertex->x = point1.x + (vec_x / 2) - (sqrt(3) * vec_y / 2);
+    vertex->y = point1.y + (sqrt(3) * vec_x / 2) + (vec_y / 2);
+    
+    return vertex;
 }
 struct point *generate_line_points(struct point point1, struct point point2, int num_points) {
     double dx = point2.x - point1.x;
@@ -34,14 +36,22 @@ struct point *generate_line_points(struct point point1, struct point point2, int
     }
     return sides;
 }
-void generate_triangle(struct triangle to_draw) {
-    struct point *sides = (struct point*) malloc(3 * to_draw.num_points * sizeof(struct point));
-    to_draw.sides[0] = generate_line_points(to_draw.vertex1, to_draw.vertex2, to_draw.num_points);
-    to_draw.vertex3 = *get_vertex(to_draw.vertex1, to_draw.vertex2);
-    to_draw.sides[1] = generate_line_points(to_draw.vertex3, to_draw.vertex2, to_draw.num_points);
-    to_draw.sides[1] = generate_line_points(to_draw.vertex3, to_draw.vertex2, to_draw.num_points);
 
+void generate_triangle(struct triangle *to_draw) {
+    to_draw->sides = (struct point**) malloc(3 * sizeof(struct point*));
+    to_draw->sides[0] = generate_line_points(to_draw->vertex1, to_draw->vertex2, to_draw->num_points);
+    
+    // Vertex 3 calculated here
+    to_draw->vertex3 = *get_vertex(to_draw->vertex1, to_draw->vertex2);
+    
+    to_draw->sides[1] = generate_line_points(to_draw->vertex2, to_draw->vertex3, to_draw->num_points);
+    to_draw->sides[2] = generate_line_points(to_draw->vertex1, to_draw->vertex3, to_draw->num_points);
 }
-void free_triangle(struct triangle to_draw){
-	free(to_draw.sides);
+
+void free_triangle(struct triangle *to_draw){
+    for (int i = 0; i < 3; i++) {
+        free(to_draw->sides[i]);
+    }
+    free(to_draw->sides);
 }
+
--- mat-geo-ques/q9/codes/plot.py
+++ mat-geo-ques/q9/codes/plot.py
@@ -1,54 +1,56 @@
 import ctypes
 import numpy as np
 import matplotlib.pyplot as plt
-class point (ctypes.Structure) :
+
+class Point(ctypes.Structure):
     _fields_ = [("x", ctypes.c_double),
-                ("y", ctypes.c_double)
-                ]
-    x = 0
-    y = 0
-class triangle (ctypes.Structure) :
-    _fields_ = [("vertex1", ctypes.Structure),
-                ("vertex2", ctypes.Structure),
-                ("vertex3", ctypes.Structure),
+                ("y", ctypes.c_double)]
+
+class Triangle(ctypes.Structure):
+    _fields_ = [("length", ctypes.c_double),
+                ("vertex1", Point),
+                ("vertex2", Point),
+                ("vertex3", Point),
                 ("num_points", ctypes.c_int),
-                ("length", ctypes.c_int),
-                ("sides", ctypes.POINTER(ctypes.POINTER(ctypes.Structure)))
-            ]
-    vertex1.x = 0
-    vertex1.y = 0
-    vertex2.x = length
-    vertex.y = 0
-    sides = ctypes.Structure * (3 * num_points)()
+                ("sides", ctypes.POINTER(ctypes.POINTER(Point)))]
 
-
-to_draw = triangle()
 gen = ctypes.CDLL('./generate.so')
-gen.get_vertex.argtypes = [ctypes.Structure, ctypes.Structure]
-gen.get_vertex.restype = ctypes.POINTER(ctypes.Structure)
-gen.generate_triangle.argtypes = [ctypes.c_double, ctypes.c_int, ctypes.POINTER(ctypes.Structure * (3 * num_points))()]
+gen.get_vertex.argtypes = [Point, Point]
+gen.get_vertex.restype = ctypes.POINTER(Point)
+gen.generate_triangle.argtypes = [ctypes.POINTER(Triangle)]
 gen.generate_triangle.restype = None
 
-# Input length and num_points
 num_points = 1000
 length = 5.0
+
+to_draw = Triangle()
 to_draw.length = length
-# you can also change vertex1 and vertex2 to change th eorientation of the triangle
-gen.get_vertex(ctypes.byref(to_draw.vertex1), ctypes.byref(to_draw.vertex2))
+to_draw.num_points = num_points
+to_draw.vertex1 = Point(0.0, 0.0)
+to_draw.vertex2 = Point(length, 0.0)
+vertex_ptr = gen.get_vertex(to_draw.vertex1, to_draw.vertex2)
+to_draw.vertex3 = vertex_ptr.contents
+
+sides_array_type = ctypes.POINTER(Point) * 3
+to_draw.sides = sides_array_type()
+
 gen.generate_triangle(ctypes.byref(to_draw))
+
+points_np = np.zeros((3, num_points), dtype=[('x', np.float64), ('y', np.float64)])
+for i in range(3):
+    for j in range(num_points):
+        points_np[i][j] = (to_draw.sides[i][j].x, to_draw.sides[i][j].y)
 
 fig = plt.figure()
 ax = plt.gca()
-points_np = np.array(to_draw.sides).reshape((3, num_points)) 
-plt.plot(points_np[0, :].x, points_np[0, :, 1].y)
-plt.plot(points_np[1, :].x, points_np[1, :].y)
-plt.plot(points_np[2, :].x, points_np[2, :].y)
-plt.annotate(f"({to_draw.vertex1.x}, {to_draw.vertex1.y})", (to_draw.vertex1.x, to_draw.vertex1.y), textcoords="offset points", xytext=(-5, -5), ha="center")
-plt.annotate(f"({to_draw.vertex2.x}, {to_draw.vertex2.y})", (to_draw.vertex2.x, to_draw.vertex2.y), textcoords="offset points", xytext=(-5, -5), ha="center")
-plt.annotate(f"({to_draw.vertex3.x}, {to_draw.vertex3.y})", (to_draw.vertex3.x, to_draw.vertex3.y), textcoords="offset points", xytext=(-5, -5), ha="center")
+for i in range(3):
+    ax.plot(points_np[i]['x'], points_np[i]['y'])
+ax.annotate(f"({to_draw.vertex1.x}, {to_draw.vertex1.y})", (to_draw.vertex1.x, to_draw.vertex1.y), textcoords="offset points", xytext=(20, 15), ha="center")
+ax.annotate(f"({to_draw.vertex2.x}, {to_draw.vertex2.y})", (to_draw.vertex2.x, to_draw.vertex2.y), textcoords="offset points", xytext=(-15, 15), ha="center")
+ax.annotate(f"({to_draw.vertex3.x}, {to_draw.vertex3.y})", (to_draw.vertex3.x, to_draw.vertex3.y), textcoords="offset points", xytext=(-5, 5), ha="center")
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
-ax.set_title('Equilateral triangle with side 5cm')
+ax.set_title('Equilateral triangle with side length 5cm')
 plt.savefig('../figs/fig.png')
 plt.show()
 
