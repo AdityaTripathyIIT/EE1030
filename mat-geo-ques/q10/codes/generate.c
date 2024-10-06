d71a5cb21ea74fff5d02919e3549184d6ee2b396
aditya tripathy <aditya@aditya.iith.ac.in>
2024-10-06 14:04:28 +0530

    q10-changes

--- mat-geo-ques/q10/codes/generate.c
+++ mat-geo-ques/q10/codes/generate.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+struct point {
+    double x;
+    double y;
+};
+struct Circle {
+    struct point center;
+    double radius;
+    int num_points;
+    struct point *points;
+};
 
-double *generate_circle_points(double center[2], double radius, int num_points) {
-    double *points = (double *)malloc(sizeof(double) * 2 * num_points);	
-    for (int i = 0; i < num_points; i++) {
-        double angle = 2 * M_PI * i / num_points;
-        points[2 * i] = center[0] + radius * cos(angle);
-        points[2 * i + 1] = center[1] + radius * sin(angle);
+void generate_circle_points(struct Circle *my_circle) {
+    my_circle->points = malloc(sizeof(struct point) * my_circle->num_points);
+    for (int i = 0; i < my_circle->num_points; i++) {
+        double angle = 2 * M_PI * i / my_circle->num_points;
+        my_circle->points[i].x = my_circle->center.x + my_circle->radius * cos(angle);
+        my_circle->points[i].y = my_circle->center.y + my_circle->radius * sin(angle);
     }
-    return points;
+}
+
+void free_circle_points(struct Circle *my_circle) {
+    if (my_circle->points != NULL) {
+        free(my_circle->points);
+        my_circle->points = NULL;
+    }
 }
 
--- mat-geo-ques/q10/codes/plot.py
+++ mat-geo-ques/q10/codes/plot.py
@@ -2,36 +2,48 @@ import ctypes
 import numpy as np
 import matplotlib.pyplot as plt
 
-circle_lib = ctypes.CDLL('./generate.so')
+class Point(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_double),
+                ("y", ctypes.c_double)]
 
-circle_lib.generate_circle_points.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_double, ctypes.c_int]
-circle_lib.generate_circle_points.restype = ctypes.POINTER(ctypes.c_double)  
-def generate_circle_points(center, radius, num_points):
-    points_ptr = circle_lib.generate_circle_points(center, radius, num_points)  
-    points = np.ctypeslib.as_array(points_ptr, (num_points, 2))
-    return points
+class Circle(ctypes.Structure):
+    _fields_ = [("center", Point),
+                ("radius", ctypes.c_double),
+                ("num_points", ctypes.c_int),
+                ("points", ctypes.POINTER(Point))]
 
-center_array_type = ctypes.c_double * 2  
-my_center = center_array_type(0.5, 0.25)  
-radius = 1/12
+gen = ctypes.CDLL('./generate.so')
+gen.generate_circle_points.argtypes = [ctypes.POINTER(Circle)]
+gen.generate_circle_points.restype = None
+
 num_points = 1000
+my_center = Point(0.5, 0.25)
+radius = 1 / 12
 
-circle_points = generate_circle_points(my_center, radius, num_points)
+to_draw = Circle()
+to_draw.radius = radius
+to_draw.num_points = num_points
+to_draw.center = my_center
 
-plt.figure()
-plt.plot(circle_points[:, 0], circle_points[:, 1], marker='o', markersize=1)
-plt.plot((my_center[0], circle_points[200, 0]),(my_center[1], circle_points[200, 1]))
-plt.annotate(f"({my_center[0]}, {my_center[1]})", (my_center[0], my_center[1]))
-plt.annotate(f"({circle_points[200, 0]:.3f}, {circle_points[200, 1 ]:.3f})", (circle_points[200, 0], circle_points[200, 1]))
-midpoint_x = (my_center[0] + circle_points[200, 0]) / 2
-midpoint_y = (my_center[1] + circle_points[200, 1]) / 2
-plt.annotate(f"Radius:{radius : .3f}", (midpoint_x, midpoint_y))
-plt.title('Points on a Circle')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.axis('equal')
+points_array_type = Point * to_draw.num_points
+to_draw.points = points_array_type()
+
+gen.generate_circle_points(ctypes.byref(to_draw))
+
+points_np = np.zeros((num_points,), dtype=[('x', np.float64), ('y', np.float64)])
+
+for j in range(num_points):
+    points_np[j] = (to_draw.points[j].x, to_draw.points[j].y)
+
+fig = plt.figure()
+ax = plt.gca()
+ax.plot(points_np['x'], points_np['y'])
+ax.annotate(f"({to_draw.center.x}, {to_draw.center.y})", (to_draw.center.x, to_draw.center.y), textcoords="offset points", xytext=(20, 15), ha="center")
+ax.plot([to_draw.center.x, to_draw.points[400].x],[to_draw.center.y, to_draw.points[400].y]);
+ax.annotate(f"Radius : {to_draw.radius : 0.3f}", ((to_draw.center.x + to_draw.points[400].x) / 2, (to_draw.center.y + to_draw.points[400].y) / 2))
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_title('Circle with given parameters')
 plt.savefig('../figs/fig.png')
-plt.grid()
 plt.show()
-
 
